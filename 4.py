d7c20ce8cb6877bbfda0202984e543033eed89f7
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-02-09 15:45:28 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,7 @@
+list1=["apple","apple","banana"]
+dict=set(list1)
+dict1=list(dict)
+count=len(dict1)
+print("The number of unique elements in list is :",count)
+
+#No of unique elements in the list
